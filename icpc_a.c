a66ff0ffc6b7f0a5fbeea210091f28d9871f2f5a
BJD765 <152183585+BJD765@users.noreply.github.com>
2024-12-04 22:03:17 +0700

    Add files via upload

---
+++ icpc_a.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int n, i, j;
+    scanf(" %d", &n);
+    char str[100];
+
+    for (i = 0; i < n; i++)
+    {
+        scanf(" %s", str);
+        char new[100];
+        strcpy(new, str);
+
+        for (j = 0; j < strlen(new) ; j++)
+        {
+            if (new[j] == 'c')
+            {
+                if(new[j + 1] == 'a' || new[j+1] == 'o' || new[j+1] == 'u')
+                {
+                    new[j] = 'k';
+                }
+                else if (new[j + 1] != 'a' && new[j+1] != 'e' && new[j+1] != 'i' && new[j+1] != 'o' && new[j+1] != 'u' && new[j+1] != 'y' && new[j+1] != 'h')
+                {
+                    new[j] = 'k';
+                }
+                else if(new[j+1] == 'e' || new[j+1] == 'i' || new[j+1] == 'y')
+                {
+                    new[j] = 's';
+                }
+                else if (new[j+1] == 'h')
+                {
+                    new[j] = 'c';
+                    new[j+1] = '1';
+                    
+                }
+            }
+            
+        }
+        for(j = 0; j < strlen(new); j++)
+        {
+            if(new[j] != '1')
+            {
+                printf("%c", new[j]);
+            }
+        }
+        printf("\n");
+        
+    }
+
+    
+    
+}
